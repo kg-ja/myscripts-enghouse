e04da27c782460f25029962da1e186335fa9e8e4
Kevin Gallimore <127315927+kg-ja@users.noreply.github.com>
2025-05-16 08:56:06 -0400

    Update gci-sbc-skew-checkandrollback.sh
    added checks for centos and rocky linux

--- gci-sbc-skew-checkandrollback.sh
+++ gci-sbc-skew-checkandrollback.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-CURRENT_TIMESTAMP=`date`
-HOST_NAME=`hostname`
+CURRENT_TIMESTAMP=$(date)
+HOST_NAME=$(hostname)
 theSerial=$(dmidecode -t system | grep Serial | awk '{print $3}')
 
 FILE=/opt/bnet/bin/runcommonenv
 SEARCH_LINE='export TGW_SIGNAL_SLEEP_TIMER_VALUE=770'
+SEARCH_LINE2='export TGW_SIGNAL_SLEEP_TIMER_VALUE=990'
 
 LOG_FILE=/tmp/GCI-SBC_SKEW-CHECK-REMOVAL-INFO-$HOST_NAME.log
 
@@ -60,8 +61,49 @@ echo "==========================================================================
 
 
 
+# Function to find os type
+os_find() {
 
-# Function to add skew value
+# Extract the OS name from /etc/os-release
+if [ -f /etc/os-release ]; then
+    os_name=$(grep ^NAME= /etc/os-release | cut -d= -f2 | tr -d '"')
+else
+    echo "OS information not found."
+    exit 1
+fi
+
+# Determine OS type
+if [[ "$os_name" == *"Rocky Linux"* ]]; then
+    OS_TYPE="rocky"
+    echo "This is Rocky OS." | tee -a $LOG_FILE
+elif [[ "$os_name" == *"CentOS"* ]]; then
+    OS_TYPE="centos"
+    echo "This is CentOS." | tee -a $LOG_FILE
+else
+    OS_TYPE="unknown"
+    echo "OS is not rocky linux for CentOS." | tee -a $LOG_FILE
+	exit 1
+fi
+}
+
+# Function to display skew value
+skew_value() {
+echo | tee -a "$LOG_FILE"
+echo "=======================================================================================" | tee -a $LOG_FILE
+echo "*SLEEP_TIMER_VALUE CHECK*" | tee -a $LOG_FILE
+cat /opt/bnet/bin/runcommonenv | grep SLEEP_TIMER_VALUE| tee -a $LOG_FILE
+echo "---------------------------------------------------------------------------------------" | tee -a $LOG_FILE
+echo | tee -a "$LOG_FILE"
+}
+
+
+
+
+
+
+
+
+# Function to remove skew value
 skew_removal() {
 
 echo "Backing up runcommonenv" | tee -a $LOG_FILE
@@ -74,7 +116,14 @@ echo "removing sleep timer value from file runcommonenv" | tee -a $LOG_FILE
 
 cd /opt/bnet/bin/
 
-sed -i '/^export TGW_SIGNAL_SLEEP_TIMER_VALUE=770$/d' runcommonenv
+if [[ "$OS_TYPE" == "centos" ]]; then
+   sed -i '/^export TGW_SIGNAL_SLEEP_TIMER_VALUE=770$/d' runcommonenv
+    echo "Skew value is now removed" | tee -a $LOG_FILE
+    
+elif [[ "$OS_TYPE" == "rocky" ]]; then
+     sed -i '/^export TGW_SIGNAL_SLEEP_TIMER_VALUE=990$/d' runcommonenv
+	  echo "Skew value is now removed" | tee -a $LOG_FILE
+fi
 
 
 
@@ -131,25 +180,32 @@ echo "==========================================================================
 }
 
 
+# Function to check skew value then call skew removal if needed
+skew_check() {
+if [[ "$OS_TYPE" == "centos" ]] && grep -Fxq "$SEARCH_LINE" "$FILE"; then
+    echo "Skew parameter value is set, action will now be taken to remove(CentOS)" | tee -a $LOG_FILE
+	skew_removal
+	skew_value
 
-
-# Main loop, will check if skew is set and correct if not
-
-if grep -Fxq "$SEARCH_LINE" "$FILE"; then
-    
-   get_server_info   
-   echo "Skew parameter value is set, action will now be taken to remove" | tee -a $LOG_FILE
-   skew_removal
-
-   echo "Rollback completed,Skew parameter has been removed" | tee -a $LOG_FILE
-
-else  
-    
-   get_server_info
-   echo "Skew parameter is not present so no action taken" | tee -a $LOG_FILE  
-   
-  
+elif [[ "$OS_TYPE" == "rocky" ]] && grep -Fxq "$SEARCH_LINE2" "$FILE"; then
+    echo "Skew parameter value is set, action will now be taken to remove(Rocky Linux)" | tee -a $LOG_FILE
+	skew_removal
+	skew_value
+	
+else
+    echo "Skew parameter is not present so no action taken" | tee -a $LOG_FILE    
+	 
+	 skew_value
 fi
+}
+
+
+
+
+# Main loop, will check if skew is set and remove
+get_server_info    
+os_find
+skew_check
 
 
 
