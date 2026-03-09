26985f80d5b8971ec8acf14f330ae9baa1f0ae79
Kevin Gallimore <127315927+kg-ja@users.noreply.github.com>
2026-03-09 08:55:46 -0400

    Add files via upload

--- sbc_diagmgr.sh
+++ sbc_diagmgr.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-LOG_FILE="/tmp/diagmgr_output.txt"
-CMD="/opt/bnet/tools/rundiagmgr"
-
 CURRENT_TIMESTAMP=$(date)
 HOST_NAME=$(hostname)
 SERIAL=$(dmidecode -t system | awk '/Serial/ {print $3}')
 IP_VM=$(ip -4 addr show eth0 | awk '/inet / {print $2}' | cut -d/ -f1 | head -n1)
 IP_HW=$(ip -4 addr show mgmt | awk '/inet / {print $2}' | cut -d/ -f1 | head -n1)
+
+LOG_FILE=/tmp/sbc_$HOST_NAME-diagmgr_output.txt
+CMD=/opt/bnet/tools/rundiagmgr
 
 touch "$LOG_FILE"
 chmod 755 "$LOG_FILE"
@@ -49,6 +49,8 @@ done
 
 echo >> "$LOG_FILE"
 echo "Run complete: $(date)" >> "$LOG_FILE"
+echo "=======================================================================================" >> "$LOG_FILE"
+echo >> "$LOG_FILE"
 echo >> "$LOG_FILE"
 
 exit 0
