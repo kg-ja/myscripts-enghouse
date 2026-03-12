8516e28c3dd43625b1c59aa6a8fecb176a061af3
Kevin Gallimore <127315927+kg-ja@users.noreply.github.com>
2026-03-12 14:19:03 -0400

    Add files via upload

--- sbc_diagmgr.sh
+++ sbc_diagmgr.sh
@@ -18,20 +18,20 @@ echo "Date: $CURRENT_TIMESTAMP" >> "$LOG_FILE"
 echo "Hostname: $HOST_NAME" >> "$LOG_FILE"
 echo "VM MGMT IP: $IP_VM" >> "$LOG_FILE"
 echo "HP Mgmt IP: $IP_HW" >> "$LOG_FILE"
-echo "---------------------------------------------------------------------------------------" >> $LOG_FILE
-echo "HW/VM -SBC-License-Platform-Details" >> $LOG_FILE
-dmidecode -t system | grep Manufacturer >> $LOG_FILE
-dmidecode -t system | grep Product >> $LOG_FILE
-dmidecode -t system | grep Serial >> $LOG_FILE
-dmidecode -t system | grep UUID >> $LOG_FILE
-echo "---------------------------------------------------------------------------------------" >> $LOG_FILE
-cat /opt/bnet/release_info >> $LOG_FILE
-echo "---------------------------------------------------------------------------------------" >> $LOG_FILE
-/opt/bnet/scripts/getVMVSystemInfo >> $LOG_FILE
-echo "---------------------------------------------------------------------------------------" >> $LOG_FILE
-/opt/bnet/scripts/swMgr Summary >> $LOG_FILE
+echo "---------------------------------------------------------------------------------------" >> "$LOG_FILE"
+echo "HW/VM -SBC-License-Platform-Details" >> "$LOG_FILE"
+dmidecode -t system | grep Manufacturer >> "$LOG_FILE"
+dmidecode -t system | grep Product >> "$LOG_FILE"
+dmidecode -t system | grep Serial >> "$LOG_FILE"
+dmidecode -t system | grep UUID >> "$LOG_FILE"
+echo "---------------------------------------------------------------------------------------" >> "$LOG_FILE"
+cat /opt/bnet/release_info >> "$LOG_FILE"
+echo "---------------------------------------------------------------------------------------" >> "$LOG_FILE"
+/opt/bnet/scripts/getVMVSystemInfo >> "$LOG_FILE"
+echo "---------------------------------------------------------------------------------------" >> "$LOG_FILE"
+/opt/bnet/scripts/swMgr Summary >> "$LOG_FILE"
 echo >> "$LOG_FILE"
-echo "---------------------------------------------------------------------------------------" >> $LOG_FILE
+echo "---------------------------------------------------------------------------------------" >> "$LOG_FILE"
 echo >> "$LOG_FILE"
 
 commands=(
